28686c4ca5abbf97f7f83cc69c5e6f36832cf042
Gagan Bindra <itsgaganbindra@gmail.com>
2013-07-11 16:12:00 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(output());
+
+var buffer = new Buffer(30);
+
+buffer = output();
+
+
+  response.send(buffer.toString("utf-8"));
 });
 
 
@@ -14,6 +20,8 @@ var infile = "index.html";
 var out = fs.readFileSync(infile);
 return out;
 };
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
