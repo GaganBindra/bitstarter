d38bf8c055bce38282b52c933b762957b2878e35
Gagan Bindra <itsgaganbindra@gmail.com>
2013-07-11 16:00:43 +0000

    Updated web.js to read from ondex.html

--- web.js
+++ web.js
@@ -3,8 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(output());
 });
+
+
+
+var output = function(){
+var fs=require('fs');
+var infile = "index.html";
+var out = fs.readFileSync(infile);
+return out;
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
